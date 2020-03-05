32ca18502d09c09b6155c699bed02a3e6e2670d7
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2020-03-05 20:46:37 +0900

    Todo一覧の表示を実装

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from todo.models import Todo
+
 
 def todo_list(request):
-    return render(request, 'todo/list.html')
+    todo_list = Todo.objects.all()
+    return render(request, 'todo/list.html', {'todo_list': todo_list})
