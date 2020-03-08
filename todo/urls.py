0bd08174f8fe514c97928d4989ceb731c10146d9
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2020-03-09 01:07:50 +0900

    編集の導線を追加

--- todo/urls.py
+++ todo/urls.py
@@ -8,4 +8,5 @@ app_name = 'todo'
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),
     path('create/', views.todo_create, name='todo_create'),
+    path('edit/<int:todo_id>/', views.todo_edit, name='todo_edit'),
 ]
