7546af0302a43c2ddb1b595b42c03a59ada8efc2
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2020-03-08 21:32:45 +0900

    Todo作成機能を実装

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField('タイトル', max_length=100)
-    done = models.BooleanField('完 / 未完')
+    done = models.BooleanField('完 / 未完', default=False)
     created_at = models.DateTimeField('作成日時', auto_now_add=True)
     updated_at = models.DateTimeField('更新日時', auto_now=True)
 
